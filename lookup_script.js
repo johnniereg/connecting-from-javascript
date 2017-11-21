8c53bc29cde16f44f28f5642fe5f836614c746c2
Johnnie Regalado <johnniereg@gmail.com>
2017-11-21 19:48:24 +0000

    Implemented print-out requirements.

--- lookup_script.js
+++ lookup_script.js
@@ -18,10 +18,19 @@ client.connect((err) => {
     return console.error("Connection Error", err);
   }
   client.query("SELECT * FROM famous_people WHERE first_name=$1 OR last_name=$1", [searchParam], (err, result) => {
+    console.log("Searching...");
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows[0]); //output: 1
+    let resultsTotal = result.rowCount;
+    console.log(`Found ${resultsTotal} person(s) by the name '${searchParam}':`);
+
+    result.rows.forEach(function(row) {
+      let dateOfBirth = row.birthdate.toISOString().slice(0,10);
+      let printString = `- ${row.id}: ${row.first_name} ${row.last_name}, born '${dateOfBirth}'`;
+      console.log(printString);
+    });
+
     client.end();
   });
 });
